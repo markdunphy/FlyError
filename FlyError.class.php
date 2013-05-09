78ad5adf71fe4714f00f79a1fc3fde411e43cf60
Mark Dunphy <markdunphy@gmail.com>
2013-05-09 19:14:45 -0400

    Bunch of untested changes to make stuff better

--- FlyError.class.php
+++ FlyError.class.php
@@ -1,88 +1,172 @@
 <?php
-/*	Store errors in the session. Displat em. Unset em. Forget em.
- *	Created by @dunphtastic
- *	http://github.com/markdunphy
- *	Use this however you want for whatever you want.  Modify it, but do not
- *  take credit as your own because that's just not cool, man. Plus, it's not
- *  even an impressive class, so why would you even bother?
- ************************************************************************/
+/**
+ * Fly Error
+ *
+ * Store errors in session. Display them. Unset them. Forget them.
+ *
+ * Use this however you want for whatever you want. Modify it, but please
+ * don't take credit as your own because that's just not cool.  Plus it's
+ * not even an impressive class so why would you even both?
+ *
+ * @author Mark Dunphy @dunphtastic ( http://github.com/markdunphy )
+ *
+ */
 class FlyError {
 
+	/**
+	 * This will contain all of your errors in a numbered array.
+	 *
+	 * @var array Reference to $_SESSION[$this->index]
+	 */
 	private $errors;
+
+	/**
+	 * @var array Reference to $_SESSION. Used for unsetting .
+	 */
 	private $session;
 
-	/* If the errors array doesn't yet exist in session, this is where it gets
-	/* created. Additionally we pass our class variable $errors as a refernce
-	/* to it for easier modification.
-	/************************************************************************/
-	function __construct() {
-		if ( !is_array($_SESSION['errors']) ) $_SESSION['errors'] = array();
-		
-		$this->errors = &$_SESSION['errors'];
-		$this->session = &$_SESSION; // So we can unset errors faster
+	/**
+	 * This is a config option. This is the index we use in the session
+	 * array to store all of the errors. Set to 'fly_error' by default
+	 * since it shouldn't interfere with anything else you're storing in there.
+	 * Change it if you'd like.
+	 *
+	 * @var string Name of the index in $_SESSION to store errors in. String
+	 */
+	private $index = 'fly_error';
+
+	/**
+	 * Constructor
+	 *
+	 * Initialize the error array in session if it doesn't exist already.
+	 * Set up references.
+	 */
+	public function __construct()
+	{
+		// Lazily create the error array in session
+		if ( !is_array( $_SESSION[$this->index] ) ) $_SESSION[$this->index] = array();
+
+		$this->errors  = &$_SESSION[$this->index];
+		$this->session = &$_SESSION;
 	}
 
-	/* Sets an array of errors in the session. ($_SESSION['errors'])
-	/* This will overwrite any other errors in session.
-	/* @param $errors - Array [Required] - An indexed array of error strings.
-	/* @param $redirect - String [Optional] - Redirect to this page or leave blank to
-									 		  not redirect at all. ex. login.php
-	/*********************************************************/	
-	public function setSessionErrors($errors, $redirect=false) {
-		// Check to make sure this is in fact an array and is not empty
-		if ( is_array($errors) && !empty($errors) ) {
+	/**
+	 * Set an array of errors in the session.
+	 *
+	 * This will overwrite any other errors in session.
+	 *
+	 * @param array $errors Required. An array of error strings.
+	 * @param string $redirect Optional. Provide a string location (e.g. errors.php)
+	 * to redirect to after setting the errors. You would most likely use this page to display them.
+	 */
+	public function set( $errors, $redirect = NULL )
+	{
+		// Check to make sure this is an array and is not empty
+		if ( $this->validate( $errors ) )
+		{
 			$this->errors = $errors;
 
-			if ( $redirect ) {
-				header("Location: ".$redirect);
-				exit();
-			}
+			if ( $redirect ) $this->redirect( $redirect );
 		}
+
+		return true;
 	}
 
-	/* Pushes ONE error onto the errors array in session. ($_SESSION['errors'])
-	/* @param $errors - String [Required] - A string to push onto the errors array.
-	/* @param $redirect - String [Optional] - Redirect to this page or leave blank to
-									 		  not redirect at all. ex. login.php
-	/*********************************************************/	
-	public function setSessionError($error, $redirect=false) {
+	/**
+	 * push method
+	 *
+	 * Pushes one error onto the stack.
+	 *
+	 * @param string $error Required. An error string to push onto the error array.
+	 * @param string $redirect Where to redirect to. Leave blank to not redirect.
+	 * @return int The index in the array where the new error is located (if no redirect supplied). False if no error supplied.
+	 */
+	public function push( $error, $redirect = NULL )
+	{
 		// Check to make sure this is in fact an array and is not empty
-		if ( !empty($error) ) {
+		if ( !empty($error) )
+		{
 			$this->errors[] = $error;
 
-			if ( $redirect ) {
-				header("Location: ".$redirect);
-				exit();
-			}
+			if ( $redirect ) $this->redirect( $redirect );
+
+			// Move array pointer to last index and return the key.
+			end( $this->errors );
+			return key( $this->errors );
 		}
-	}
-
-
-	/* Print out all errors in $_SESSION['errors'] each on it's own line
-	/* @param $unsetAll - Boolean [Optional] - True if you want to unset all of the errors
-											   when you're done printing them. False otherwise.
-	/*********************************************************/	
-	public function displaySessionErrors($unsetAll=true) {
-		foreach ( $this->errors as $err ) {
-			print $err.'<br />';
-		}
-
-		if ( $unsetAll ) $this->unsetAllErrors();;
-	}
-
-	/* Unset all errors stored in the session
-	/*********************************************************/	
-	public function unsetAllErrors() {
-		unset($this->session['errors']);
-	}
-
-	/* Do we have any errors set?
-	/* @return Boolean - True if there are errors, false otherwise.
-	/*********************************************************/	
-	public function hasErrors() {
-		if ( !empty($this->errors) ) return true;
 
 		return false;
+	}
+
+	/**
+	 * Display method
+	 *
+	 * Display all of the errors in the array.
+	 *
+	 * @param boolean $unset Whether to unset the errors after displaying them. True by default.
+	 */
+	public function display( $unset = true )
+	{
+		if ( !$this->validate( $this->errors ) ) return false;
+
+		print implode( '<br />', $this->errors );
+
+		if ( $unset ) $this->unsetAll();
+	}
+
+	/**
+	 * Unset all method
+	 *
+	 * Unset all errors stored in the session
+	 */
+	public function unsetAll()
+	{
+		unset($this->session[$this->index]);
+	}
+
+	/**
+	 * Has errors method
+	 *
+	 * Do we have any errors set?
+	 *
+	 * @return boolean True if we have errors, false if not.
+	 */
+	public function hasErrors()
+	{
+		if ( $this->validate( $this->errors ) ) return true;
+
+		return false;
+	}
+
+	/**
+	 * Redirect method
+	 *
+	 * Used for redirecting to a new page.
+	 * @param string $location Required. Where to redirect to.
+	 * @return boolean False if no location is set. Otherwise void.
+	 */
+	private function redirect( $location = NULL )
+	{
+		if ( $location )
+		{
+			header("Location: $location");
+			exit();
+		}
+
+		return false;
+	}
+
+	/**
+	 * Array validate method
+	 *
+	 * Validate an array. Checks if it is an array and if it is empty.
+	 *
+	 * @param array $array Reuired. The array to check.
+	 * @return boolean Returns true if is array and not empty.
+	 */
+	private function validate( $array = NULL )
+	{
+		return ( is_array( $array ) && !empty( $array ) );
 	}
 
 }
