b54a05712a50696c2d38df261d805b96fb1535dd
Mark Dunphy <markdunphy@gmail.com>
2013-07-06 13:39:17 -0400

    move FlyError to src folder. Add bootstrap file for phpunit. New tests.

--- tests/FlyErrorTest.php
+++ tests/FlyErrorTest.php
@@ -1,25 +1,52 @@
 <?php
-require __DIR__ . '/../src/FlyError.php';
 
 class FlyErrorTest extends PHPUnit_Framework_TestCase {
 
+	/**
+	 * Instantiate a new FlyError object
+	 * Initialize $_SESSION to an array so TravisCI doesn't
+	 * get upset.
+	 */
 	public function setUp()
 	{
 		$_SESSION  = array();
 		$this->fly = new FlyError;
 	}
 
+	/**
+	 * Test that our errors array is indeed an array
+	 */
 	public function testArray()
 	{
 		$this->assertInternalType( 'array', $this->fly->all() );
 	}
 
+	/**
+	 * Test pushing a single error onto the stack
+	 */
 	public function testPushSingle()
 	{
 		$error = 'Failed to validate username!';
 		$this->fly->push( $error );
 
-		$this->assertEquals( $error, $this->fly->get(0) );
+		$this->assertEquals( $error, $this->fly->get() );
+		$this->fly->clear(); // Clear errors for next test
+	}
+
+	/**
+	 * Test pushing an array of errors onto the stack
+	 */
+	public function testPushMultiple()
+	{
+		$errors = array(
+			'Failed to validate username!',
+			'Password is not long enough!',
+			'A vaild email must be supplied'
+		);
+
+		$this->fly->push( $errors );
+
+		$this->assertEquals( 3, count( $this->fly->all() ) );
 	}
 
 }
